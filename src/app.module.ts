fed1f01ac32e17d00597af0c8c0e457a7b8a4805
Rafael Hentz <rafael.hentz@unoesc.edu.br>
2022-11-10 12:56:50 -0300

    Criado resource Clientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientesModule } from './clientes/clientes.module';
+import { Cliente } from './clientes/entities/cliente.entity';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ClientesModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'sonoscompiuter',
+    entities: [Cliente],
+    synchronize: true,
+  }),],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
